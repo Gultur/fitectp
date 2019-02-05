8907fc0f7b7a484944accd445c1e547600e4f67d
Nicolas <monsieur_yan@hotmail.com>
2019-02-05 10:39:26 +0100

    Add Tests Skeleton for AccountController

---
+++ tests/ContosoUniversity.Tests/Controllers/AccountControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using ContosoUniversity.Controllers;
+using ContosoUniversity.DAL;
+using ContosoUniversity.Models;
+using ContosoUniversity.Tests.Tools;
+using Moq;
+using NUnit.Framework;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+
+namespace ContosoUniversity.Tests.Controllers
+{
+    class AccountControllerTests: IntegrationTestsBase
+    {
+        private MockHttpContextWrapper httpContext;
+        private AccountController controllerToTest;
+        private SchoolContext dbContext;
+
+        [Test]
+        public void Login_InvalidCredential_Fail()
+        {
+            Assert.That(false);
+        }
+
+        [Test]
+        public void Login_ValidLoginInvalidPawword_Fail()
+        {
+            Assert.That(false);
+        }
+
+        // A completer
+    }
+}
